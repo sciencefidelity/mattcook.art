2ae0f86660222f52a5033a2c8a89434a246791ec
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-15 23:52:51 +0100

    refactor: try some different button styles

--- styled/src/pages/Index.tsx
+++ styled/src/pages/Index.tsx
@@ -11,14 +11,43 @@ const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
 `
+// background: ${props => props.primary ? "white" : "palevioletred"}
+// color: ${props => props.primary ? "white" : "palevioletred"}
+const Button = styled.button`
+  background-color: palevioletred;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  text-decoration: none;
+`
+
+const TomatoButton = styled(Button)`
+  background-color: tomato;
+  border-color: tomato;
+`
+
+const ReversedButton = props => (
+  <Button
+    {...props}
+    children={props.children.split("").reverse()}
+  />
+)
 
 const Index: FC = () => {
   return (
-    <Wrapper>
-      <Title>
-        Hello World!
-      </Title>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Title>
+          Hello World!
+        </Title>
+      </Wrapper>
+      <Button>Normal</Button>
+      <TomatoButton as="a" href="#">Tomato</TomatoButton>
+      <Button as={ReversedButton}>Primary</Button>
+    </>
   )
 }
 export default Index
