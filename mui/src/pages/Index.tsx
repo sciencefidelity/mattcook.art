7d32157bd939d4412a286457afc47fcb3058f75a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-16 21:20:54 +0100

    refactor: try an autocomplete

--- mui/src/pages/Index.tsx
+++ mui/src/pages/Index.tsx
@@ -1,14 +1,26 @@
 import React, { FC } from "react"
 import Button from "@mui/material/button"
+import TextField from "@mui/material/TextField"
+import Autocomplete from "@mui/material/Autocomplete"
+import { top100Films } from "../data/top100films"
 
 const Index: FC = () => {
   return (
     <div className="container">
       <main>
-        <Button variant="contained">Dark by default</Button>
+        <Button variant="contained">Hello Button</Button>
+        <hr />
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={top100Films}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Movie" />}
+        />
         <p>This app is using the dark mode</p>
       </main>
     </div>
   )
 }
 export default Index
+
